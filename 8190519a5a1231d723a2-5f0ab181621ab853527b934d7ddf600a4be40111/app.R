19c7e6c44186962d03ed79cfdcea3fe8a344508b
Mark Clements <scartomyzon@gmail.com>
2016-12-05 08:58:25 -0500

    added bio215 palms and parrots lifetable data

---
+++ 8190519a5a1231d723a2-5f0ab181621ab853527b934d7ddf600a4be40111/app.R
@@ -0,0 +1,18 @@
+## app.R
+ui <- shinyUI(fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("obs", "Number of observations:",
+                  min = 10, max = 500, value = 100)
+    ),
+    mainPanel(plotOutput("distPlot"))
+  )
+))
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+  })
+}
+
+shinyApp(ui = ui, server = server)
